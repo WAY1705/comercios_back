4e9e21bc5e555f68c33ccbf7dda9c1b2e21189e6
Carlosr08 <crivascyt@gmail.com>
2022-11-11 15:44:14 -0400

    cambios v2

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import cors from 'cors';
 
 import AuthController from './routes/auth'
-import PhotoController from './routes/photo'
+// import PhotoController from './routes/photo'
 
 // Initializations
 const app: Application = express();
@@ -19,7 +19,7 @@ app.use(cors());
 
 // Routes
 app.use('/api/auth', AuthController);
-app.use('/api/photo', PhotoController);
+// app.use('/api/photo', PhotoController);
 
 // this folders for this application will be used to store public file images
 app.use('/uploads', express.static(path.resolve('uploads')));
